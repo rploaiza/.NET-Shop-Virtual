5f70a1fd9e32cc7aa3c71b63b14c74f3a3408a53
Unknown <pauloaiza@hotmail.es>
2018-02-23 11:30:05 +0100

    Initial commit

---
+++ TiendaVirtual/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TiendaVirtual.Controllers
+{
+    public class HomeController : Controller
+    {
+        private tienda_virtual2018Entities db = new tienda_virtual2018Entities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.Producto.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
